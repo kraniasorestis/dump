874b746e6a2c27b8c995d9528136b1a6e82bec1c
Kranias Orestis <kraniasorestis@protonmail.com>
2020-02-29 23:43:50 +0200

    Update Numbers.py

--- src/Numbers.py
+++ src/Numbers.py
@@ -1,13 +1,22 @@
-from Auxiliary import combine2
+import Auxiliary
 
-def add_nums(l):   # add some numbers to the back a list's elements
+def add_nums(l):   # adds some numbers to the back a list's elements
     tmp = []
     for i in range(0, 10):
-        tmp.append('0'+str(i))
         tmp.append('_'+'0'+str(i))
         tmp.append('-'+'0'+str(i))
+        tmp.append('.'+'0'+str(i))
     for i in range(0, 100):
         tmp.append(str(i))
         tmp.append('_'+str(i))
         tmp.append('-'+str(i))
-    return combine2(l, tmp)
+        tmp.append('.'+str(i))
+    return Auxiliary.combine(l, tmp)
+
+def add_nums2(l):       # adds some numbers both in the front and back of the list's items
+     tmp = []
+     for i in range(0, 10):
+        tmp.append('0'+str(i))
+     for i in range(0, 100):
+        tmp.append(str(i))
+     return Auxiliary.combine2way(l, tmp)
