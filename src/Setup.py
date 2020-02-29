e9b2f86e8ea0516657ac8813342ef40cd29b5ebd
Kranias Orestis <kraniasorestis@protonmail.com>
2020-02-29 23:46:34 +0200

    Update Setup.py

--- src/Setup.py
+++ src/Setup.py
@@ -7,16 +7,18 @@ minchar = 6
 maxchar = 16
 
 # This range of years will be combined with names etc in the passwords
-years = list(range(1980, 2019))
+years = list(range(1980, 2021))
 
-# Same goes for those number sequences
+# Same goes for those number sequences and other numbers
 sequences = [
                 0,00,000,0000,00000,000000,0000000,00000000,000000000,
-                123,1234,12345,123456,123123,1234567, 12345678,123456789,123456789,
+                123,1234,12345,123456,123123,1234567, 12345678,123456789,
                 135,1357,13579,1357911,135791113, # Odd numbers
                 246,2468,246810,24681012,2468101214, # Even numbers
                 235,2357,235711,23571113,2357111317, # Prime numbers
-                112,1123,11235,112358,11235813,1123581321 # Fibonachi sequence
+                112,1123,11235,112358,11235813,1123581321, # Fibonachi sequence
+				131313,123123,121212,6969,696969,112233,233444,2334445555, # Other usual numbers in passwords 
+                233444555566666,232323 # Other usual numbers in passwords
             ]
 
 # This dictionary will replace the vowels and diphthongs on the left with the ones on the right
@@ -24,6 +26,7 @@ leet_dict = {
             	'a':'@',
                 'w':'o',
                 'o':'0',
+                'e':'3',
                 'gk':'g',
                 'gg':'g',
                 'mp':'b',
@@ -31,10 +34,9 @@ leet_dict = {
                 'h':'i',
                 'y':'i',
                 'u':'i',
+                'v':'7',
                 'ei':'i',
-                'f':'u',
-                'v':'u',
-                'ay':'ef',
+                'ay':'af',
                 'ey':'ef',
                 'au':'af',
                 'eu':'ef',
@@ -45,68 +47,154 @@ leet_dict = {
                 'nn':'n',
                 'll':'l',
                 'pp':'p',
-                'mm':'m'
+                'mm':'m',
+                'rr':'r',
+                'tt':'t'
             }
+
+leet_dict_2 = {
+            	'a':'4',
+                'w':'0',
+                'gk':'gg',
+                'gg':'gk',
+                'oy':'ou',
+                'ay':'av',
+                'ey':'ev',
+                'au':'av',
+                'eu':'ev',
+                't':'7',
+                'x':'ks',
+                'ch':'x'
+                 }
+
+
 # Special characters to be added in the passwords
-SpecChars = ['!', '.', '@', '#', '$', '%', '*']
+SpecChars = ['!', '!!', '!!!', '.', '..', '...', '@', '#', '$', '%', '*']
+
 
 # Specific keywords to be used with the favourite-team option
 team_dic = {
-        1: ['thrillos', 'gayros', 'olympiakos', 'olympiakara', 'thira', 'gate', 'thrilara', 'thira7', 'gate7', '7', 'oeo', 'ole'],
+        1: ['thrillos', 'gayros', 'olympiakos', 'olympiakara', 'thira', 'gate', 'thira7', 'gate7', '7', 'oeo', 'ole'],
         2: ['panatha', 'panathinaikos', 'thira', 'gate', 'trifili', 'trifilara', 'vazelos', 'vazela', 'thira13', 'gate13', '13', 'oeo', 'ole'],
-        3: ['original', 'aek', 'original', 'aek', 'aekara', 'thira', 'aekara', 'hanoumi', 'hanoumaki', 'thira21', 'gate21', '21', 'oeo', 'ole'],
+        3: ['original', 'aek', 'original', 'aekara', 'thira', 'aekara', 'hanoumi', 'hanoumaki', 'thira21', 'gate21', '21', 'oeo', 'ole'],
         4: ['paok', 'toumpa', 'paoki', 'mpaok', 'paokara', 'mpaokara', 'voulgaroi', 'salonika', 'thessaloniki', 'gate', 'thira', 'thira4', 'gate4', '4', 'oeo', 'ole'],
         5: ['alogaki', 'visini', 'larisa', 'larsa', 'trakter', 'ael', 'aelara', 'alogaki', 'oeo', 'ole'],
-        6: ['skouliki', 'salonika', 'xarilaou', 'aris', 'arianos', 'oeo', 'ole' ]
+        6: ['skouliki', 'salonika', 'xarilaou', 'aris', 'arianos', 'oeo', 'ole']
     }
 
-# Common passwords derived from the "worst 500 passwords" wordlist hosted in SkullSecurity. 
+# Common passwords derived from the "worst 500 passwords" wordlist hosted in SkullSecurity.
 # I deleted a lot that were just English-only names.
-# I tried to produce the greek counterparts of a lot that could be being used in Greece
+
 pop_pswd = [
-            'dragon','iloveyou','love','princess','master','monkey', '121212','podosfairo','abgrtyu','dreams',
+            'dragon','iloveyou','love','princess','master','monkey','podosfairo','abgrtyu','dreams','extreme',
             'qwerty','abcdef','asdf','qazwsx','1qaz2wsx','abc123','qwer','zxcasd','asdzxc','qweasdzxc','password',
-            'login','admin','root','paremoumiapipa','arxidi','arxidia','malakas','mpastardos','kargiolhs','kargiolis',
-            'pussy','dragon','696969','mustang','kwdikos','kodikos','letmein','baseball', 'mpala',
-            'master','football','shadow','monkey','abc123','pass','fuckme','6969','jordan','harley','mousikh','tester',
-            'ranger','iwantu','fuck','test','batman','tigger','access','love','buster','olympiakos',
-            'olimpiakos','gayros','panathinaikos','aek','ael','aris','paok','mpasket','basket','hockey','killer','george',
-            'sexy','superman','spiderman','wolverine','cyclops','asshole','fuckyou','summer','hammer','enter','nicole',
-            'cowboy','fucker','corvette','freedom','blowjob','yellow','kitrino','camaro','secret','dick','poutsos','poutsa',
-            '131313','123123','bitch','hello','please','porsche','guitar','chelsea','black','diamond','nascar',
-            'computer','wizard','magos','money','lefta','fragka','iceman','tigers','purple','andrea','horny', 'dakota','player','paixths','paixtaras','paixtoura','starwars','boomer','cowboys','charles','girls','gkomenakia','gkomenes',
-            'coffee','kafes','bulldog','john','mpyra','johnny','johnaras','gandalf','winter','brandy','tennis','mike','pipa',
-            'pipes','ferrari','ferari','maverick','diablo','sexsex','hardcore','counterstrike','counter','chris','yamaha','driver',
-            'angels','maddog','butthead','fucking','captain','bigdick','xavier','viking','snoopy','blue','mple','eagles',
-            'winner','house','flower','united','zxcvbn','golf','bond007','tiger','doctor','angel','junior','porno','athens',
-            'thessalonikh','thessaloniki','salonica','patra','larisa','larissa','trikala','karditsa','volos',
-            'hrakleio','xania','krhth','rethymno','thiva','lamia','leivadia','livadia','kozani','kozanh','grevena','ptolemaida','drama','serres',
-            'seres','kavala','alexandroupolh','rodos','xalkida','evoia','katerinh','katerini','sparth','tripolh','badboy','gamwthnpanagia',
-            'spider','1212','porn','matrix','teens','scooby','jason','cumshot','lover','princess','mercedes','doggie','zzzzzz',
-            'horney','2112','fred','xxxxx','tits','member','boobs','bigdaddy','penis','voyager','white','topgun','bigtits','bitches','green','prasino',
-            'portokali','mwv','mob','super','qazwsx','magic','slayer','asdf','video','kwlos','kwlarakia','kwlaraki','skata','marlboro',
-            'internet','action','monster','jeremy','bill','crystal','peter','pussies','cock','beer','rocket','prince','beach','amateur',
-            'star','testing','murphy','mother','forever','angela','viper','lovers','suckit','gregory','nikolas','nikos','paylos',
-            'kwstas','giwrgos','apostolis','giannhs','grhgorhs','grigoris','dhmhtrhs','dimitris','mhtros','mitsos','buddy','filos','kollhtos',
-            'kolitos','kolhth','kolith','whatever','young','nicholas','lucky','helpme','midnight','baby','cunt','startrek','232323','bigcock',
-            'happy','sophie','ladies','naughty','giants','booty','blonde','fucked','golden','fire','sagapw','sagapo','agaph',
-            'fwtia','einstein','warrior','mouni','kayli','kayla','xynw','xysia','xysi','slut','xanthia','xan8ia','nipples','power','victoria','roges',
-            'roga','vizia','vyzia','vizi','vyzi','vagina','toyota','hotdog','gamhsi','gamhsou','gamiese','gamiesai','gamias',
-            'gamaw','segamaw','gamhseme','gamame','rock','extreme','erotic','erwtas','dirty','ford','arsenal','wolf','nipple','alex',
-            'dota','legend','movie','success','jaguar','great','cool','japan','naked','squirt','thalassa','apple','alexis','swimming',
-            'dolphin','kolypmi','casper','stupid','shit','saturn','gemini','apples','august','cumming','kitty','rainbow','112233','cream','shaved',
-            'mine','delfini','king','racing','aetos','eagle','hentai','little','kokkino','cocacola','animal','private','blondes','melaxrines',
-            'enjoy','girl','apollo','qwert','time','metal','women','gynaikes','voodoo','magnum','juice','oneira','music','mistress','phantom',
+            'login','admin','root','mustang','kwdikos','kodikos','letmein','baseball','hockey','dirty','ford',
+            'master','football','shadow','monkey','abc123','pass','fuckme','jordan','harley','mousikh','tester',
+			'ranger','iwantu','test','batman','tigger','access','love','buster','mpala','mpasket','basket','killer',
+            'sexy','superman','spiderman','wolverine','cyclops','summer','hammer','enter','women','voodoo','magnum',
+            'cowboy','corvette','freedom','blowjob','yellow','camaro','secret','gamatos','legend','movie','rock',
+            'hello','please','porsche','guitar','chelsea','black','diamond','success','jaguar','great','cool','hotdog',
+            'computer','wizard','magos','money','lefta','fragka','iceman','tigers','purple','arsenal','wolf',
+			'dakota','player','paixths','paixtaras','paixtoura','starwars','boomer','cowboys','charles','girls','coffee',
+            'bulldog','john','mpyra','johnny','johnaras','casper','stupid','saturn','juice','oneira','music','phantom',
+			'gandalf','winter','brandy','tennis','mike','ferrari','ferari','maverick','sexsex','butterfly','btrfly',
+			'hardcore','counterstrike','counter','yamaha','driver','angels','maddog','captain','erotic','erwtas',
+			'xavier','viking','snoopy','blue','eagles','winner','house','flower','united','zxcvbn','golf','toyota',
+			'bond007','tiger','doctor','angel','junior','athens','thessalonikh','thessaloniki','salonica','patra',
+			'larisa','larissa','trikala','karditsa','volos','hrakleio','xania','krhth','rethymno','thiva','lamia','leivadia',
+			'livadia','kozani','kozanh','grevena','ptolemaida','drama','serres','seres','kavala','alexandroupolh','rodos',
+			'xalkida','evoia','katerinh','katerini','sparth','tripolh','badboy','spider','1212','rainbow','kitty',
+			'matrix','teens','scooby','scooobydoo','lover','princess','mercedes','zzzzzz','2112','gemini','apples','august',
+			'member','bigdaddy','voyager','white','topgun','enjoy','girl','apollo','blondes','cocacola','animal','private',
+			'green','super','qazwsx','magic','slayer','asdf','video','eagle','little','mine','king','racing','naked',
+			'marlboro','internet','action','monster','crystal','qwert','time','metal','cream','shaved','thalassa',
+			'beer','rocket','prince','beach','amateur','star','testing','murphy','mother','forever','angela','viper',
+			'lovers','buddy','kollhtos','kolitos','kolhth','kolith','whatever','young','lucky','helpme','japan',
+			'midnight','baby','startrek','happy','sophie','ladies','naughty','giants','booty','apple','swimming','dolphin',
+			'blonde','golden','fire','sagapw','sagapo','agaph','einstein','warrior','power','victoria',
+			'12three','12tria','123four','123tessera','123tesera','1234five','1234pente','12345six','12345eksi','1233456seven',
+            '123456epta','1234567eight','1234567oktw',
             ]
 
-######## PLACEHOLDERS for upcomming features ########
+gaming = [
+			'playstation','psx','nintendo','nes','snes','gameboy','gameboycolor','sega','segasaturn','saturn','megadrive',
+            'mastersystem',
+            
+            'minecraft','hearthstone','csgo','fortnite','lol','leagueoflegends','wow','warcraft','overwatch','mario','dota2',
+			'dota','starcraft','diablo','pokemon','pubg',
+   
+            'zelda','crashbandicoot','crash','coco','cocobandicoot',
+
+]
+
+vulgar = ['paremoumiapipa','arxidi','arxidia','malakas','mpastardos','kargiolhs','kargiolis','pussy','mounopano',
+          'fuck','asshole','fuckyou','fucker','dick','poutsos','poutsa','bitch','kaula','gkaula','gkavla','eatadick',
+          'horny','kavla','gkomenakia','gkomenakia','mounia','moynia','moynara','mounara','pipa','pipes','butthead',
+          'fucking','porn','brazzers','youporn','pornhub','bdsm','bondage','gamwthnpanagia','horney','cumshot','doggie',
+          'bigtits','bitches','kwlaraki','kwlos','kwlarakia','nipple','tits','vyzia','bigcock','suckit','boobs','cunt',
+          'mouni','kayli','cumming','gamhsi','gamhsou','gamiese','gamiesai','gamias','gamaw','segamaw','gamhseme',
+          'hentai','gamame','xynw','xysia','xysi','slut','squirt','nipples','pussies','cock','penis','skata','fucked',
+          'mistress','shit','vizia','vyzia','vizi','vyzi','vagina'
+
+]
 
 
-# def swearing():
-#     lista vrisiwn
+nic_dic = {
+        'giwrgos':['giwrgaras','giwrgakhs','giwrgaros','tzwrhs','tzwrtzhs','giwrgakos','gewrgios','george'],
+        'giannhs':['giannakos','tzonnhs','johnny','john','tzwnaras','iwannhs'],
+        'kwstas':['kwnstantinos','kwtsos','kwstaras','gus','kwstakhs','kwstakos','ntinos'],
+        'manwlhs':['manwlakhs','manwlakos','manwlios','emmanouhl','manwlos','manwlaras'],
+        'vaggelhs':['vaggelakos','vaggelaras','vaggelakhs','evanggelos','vaggos'],
+        'dhmhtrhs':['takhs','jimhs','tzimhs','tzimakos','tzimaras','dhmhtrakos','dhmhtrakhs','mhtsos','mhtsaras','mhtsaki','jimmy','jim','jimmhs','dhmhtrios'],
+        'nikos':['nikolas','nick','nikolos','nikolakhs','nikolaos'],
+        'panagiwths':['panos','panoulhs'],
+        'vasilhs':['vasilakhs','vasilaras','vasilakos','bill','billy','mpillhs','vasileios'],
+        'xrhstos':['xrhstaras','xrhstakhs','xrhstakos','kitsos','chris'],
+        'thanashs':['nasos','sakhs','thanasaras','thanasakos','thanasakhs','athanasios'],
+        'mixalhs':['mixalakhs','mixalakos','mixalaras','mixahl'],
+        'alexandros':['alexandrakos','alexandroylhs','alekos','aleka','alex'],
+        'spyros':['spyro','spyridwn','pipphs','spyrakos','spyrothedragon'],
+        'antwnhs':['antwnakhs','antwnaras','antony','antwnios','antwnaros','tony'],
+        'leyterhs':['eleytherios','terry','leyterakos','lefterakhs'],
+        'stefanos':['stefos','stefanakos','stefanaras','stephan'],
+        'anastashs':['anastasios','tasos','tasoulhs','soulhs'],
+        'thodwrhs':['theodwros','dwrhs','dwros','thodwros','thodwras','thodwrakos','theodore'],
+        'hlias':['liakos','lias'],
+        'antreas':['andrew','antrakos','antrikos','adnreas','andrakos','andrikos'],
+        'apostolhs':['apostolakos','apostolakhs','apostolaras','apostolaros','tolhs','tolakos','tolaras','apostolos'],
+        'xaralampos':['mpamphs','mpampinos','xampos','xaralampakos','xaralamphs','xaralampara','xaralampoylhs'],
+        'stayros':['stayrakas'],
+        'petros':['petrakos','petroulhs','petrakhs','petraras','petran','peter'],
+        'stelios':['stelaras','stelakos','stylianos'],
+        'swthrhs':['swthrakhs','swthraras','swthrakos','swtos'],
+        'thwmas':['thwmakos','thwmaras','tom','tommy'],
+        'fwths':['fwtaras','fwtakhs','fwtoulhs','fwtios'],
+        'grhgorhs':['grhgorios','gregory','greg'],
+        'maria':['maraki','maroula','mary','mairh'],
+        'elenh':['elenaki','lena','elenara','elenitsa','leniw','lenaki'],
+        'katerina':['katernaki','katerinoula','katerinara','kateriniw','rina','rinaki','riniw','rinoula','aikaterinh'],
+        'vasilikh':['vasw','vasilikoyla','vasoula'],
+        'sofia':['sofoula','sofaki','sofy','sophy'],
+        'anastasia':['anastasaki','anastasoyla','tasoyla','soula'],
+        'evaggelia':['evanggelia','litsa','litsaki','litsara','vaggeliw','vaggelitsa','vanggeliw','vangellitsa'],
+        'iwanna':['gianna','joanna','josh','giannoyla'],
+        'eirhnh':['eirhnaki','eirhnoula','eirhnara','rhniw','rhna','peace','rhnaki','rhnoula'],
+        'panagiwta':['giwta','giwtaki','giwtoula','giwtara','panagiwtaki','panagiwtara','panagiwtoula'],
+        'xristina':['xristinaki','xristinara','xristinel','xristinoyla','christy'],
+        'anna':['annoula','annaki','ann','anny'],
+        'despoina':['despoinaki','despoinoyla'],
+        'fwteinh':['fanh','fwteinaki','fwteinoula','fanitsa'],
+        'alexandra':['aleka','alex','alexandroula','alexandraki'],
+        'athhna':['athnoula','athhnaki','athens'],
+        'nikoletta':['nicole','nikole','nikol','nikolettitsa','nikolettoula'],
+        'aggelikh':['aggelikoula','aggela'],
+        'gewrgia':['gwgw','gwgaki','gewrgitsa','gewrgoyla'],
+        'dhmhtra':['jimhs','tzimhs','tzimakos','tzimaras','dhmhtrakos','dhmhtrakhs','mhtsos','mhtsaras','mhtsaki','jimmy','jim','jimmhs','dhmhtrios','dhmhtroula','dhmhtraki','mhtsaina'],
+        'kwnstantina':['kwnstantinaki','kwnstantinoula','konstance','constance','konstanta','ntina','ntinaki','kwnstantinos','kwtsos','kwstaras','gus','kwstakhs'],
+        'paraskeyh':['paraskeyoyla','voula','evh','evita','paraskeyaki']
+}
 
-# def komma():
-#     analogo me thn omada
 
-# def fav_chars
-#     lista agaphmenwn xarakthrwn analoga me tainies/seires pou vlepei
+
+
+
